df83d9610b7ddeba943e54acf01d41602d9f4db7
dav <usr21p1@gmail.com>
2023-07-08 20:17:42 -0700

    Add hard-coded typescript calls.

--- bath-signal/demo/bathSignal.ts
+++ bath-signal/demo/bathSignal.ts
@@ -73,3 +73,101 @@ type ResCheckMailbox = {
   error: CheckMailboxError | null;
   messages: MailboxMessage[] | null;
 };
+
+async function create_call() {
+  let fetchRes = await fetch(
+    "https://bath-signal-test.onrender.com/api/call/create_call",
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    },
+  );
+  let res = await fetchRes.text();
+  return JSON.parse(res);
+}
+
+async function join_query(req: JoinQuery): Promise<ResJoinQuery> {
+  let fetchRes = await fetch(
+    "https://bath-signal-test.onrender.com/api/call/join_query",
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    },
+  );
+  let res = await fetchRes.text();
+  return JSON.parse(res);
+}
+
+async function send_offer(req: SendOffer): Promise<ResSendOffer> {
+  let fetchRes = await fetch(
+    "https://bath-signal-test.onrender.com/api/call/send_offer",
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(req),
+    },
+  );
+  let res = await fetchRes.text();
+  return JSON.parse(res);
+}
+
+async function send_answer(req: SendAnswer): Promise<ResSendAnswer> {
+  let fetchRes = await fetch(
+    "https://bath-signal-test.onrender.com/api/call/send_answer",
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    },
+  );
+  let res = await fetchRes.text();
+  return JSON.parse(res);
+}
+
+async function send_ice(req: SendICE): Promise<ResSendICE> {
+  let fetchRes = await fetch(
+    "https://bath-signal-test.onrender.com/api/call/send_ice",
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    },
+  );
+  let res = await fetchRes.text();
+  return JSON.parse(res);
+}
+
+async function check_mailbox(req: CheckMailbox): Promise<ResCheckMailbox> {
+  let fetchRes = await fetch(
+    "https://bath-signal-test.onrender.com/api/call/check_mailbox",
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(req),
+    },
+  );
+  let res = await fetchRes.text();
+  return JSON.parse(res);
+}
